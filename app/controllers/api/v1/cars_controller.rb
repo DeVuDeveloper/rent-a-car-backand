041540ab331523f353c09604e2489a1947cdc2a8
VuDej <drvu47@gmail.com>
2022-05-26 08:28:25 +0200

    added api controllers

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,2 +1,35 @@
-class Api::V1::CarsController < ApplicationController
+class Api::V1::RoomsController < ApplicationController
+  before_action :set_car, only: %i[show destroy]
+
+  def index
+    @cars = current_user.cars.all
+  end
+
+  def create
+    @car = current_user.cars.new(car_params)
+
+    if @car.save!
+      render :create, status: :created
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @car.destroy
+      render json: { message: 'Car has been successfully deleted' }
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:name, :fuel, :seats, :navigation, :type, :photo, :price_for_day, :city, :user_id)
+  end
 end
