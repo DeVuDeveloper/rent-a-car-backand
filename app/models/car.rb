4e65fd75a1bd17ed6e7b56b6a859ce728aac79eb
VuDej <drvu47@gmail.com>
2022-05-26 08:50:29 +0200

    created json views

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,6 @@ class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
   validates :name, :city, length: { maximum: 10 }
-  validates :name, :fuel, :seats, :type, :photo, :price_per_day, :city, presence: true
+  validates :name, :fuel, :seats, :navigation, :type, :photo, :price_per_day, :city, presence: true
   validates :price_for_day, numericality: { greater_than_or_equal_to: 0 }
 end
-
---
+++ app/views/layouts/api/v1/reservations/create.json.builder
@@ -0,0 +1,8 @@
+json.reservation do
+    json.id @reservation.id
+    json.pick_up_date @reservation.check_in
+    json.drop_off_date @reservation.check_out
+    json.car_id @reservation.room_id
+    json.user_id @reservation.user_id
+    json.created_at @reservation.created_at
+  end
